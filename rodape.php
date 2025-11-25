e944f28b053bc168ce2e58a8ef6db07a106be85b
RafaelAlustau <rribeiro0604@icloud.com>
2025-11-25 16:21:43 -0300

    Add files via upload

---
+++ rodape.php
@@ -0,0 +1,23 @@
+<footer class="rodape">
+   
+    <h3>Contato</h3>
+
+    <h2>Fale comigo agora: </h2>
+
+    <form action="mensagem" method="post">
+        <label for="text">Seu nome: </label>
+        <input type="text" name="nome" id="nome">
+        <label for="text">Mensagem: </label>
+        <input type="text" name="mensagem" id="mensagem">
+        <input type="submit" value="Enviar">
+
+    </form>
+    <a href="#topo">Ir para o topo</a>
+
+
+    <p>© 2025 Academia Mataraca — Todos os direitos reservados</p>
+</footer>
+
+
+</body>
+</html>
