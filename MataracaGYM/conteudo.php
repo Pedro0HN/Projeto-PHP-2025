eea98e589d662e55e76f163535bf7253e3ed8c47
José Guilherme Ferreira Alves <jose.alves007@cs.unipe.edu.br>
2025-12-03 19:59:32 -0300

    ICONE UPDATE
    pronto pro merge com a main

---
+++ MataracaGYM/conteudo.php
@@ -0,0 +1,18 @@
+
+
+<nav class="conteudo">
+    
+    <h1>
+        Academia Mataraca <br>
+        <img class='imagem' src="logo_mataraca.jpeg" alt="logo" width = 300px>
+         <br>
+    </h1>
+    <h2>Conheça nossos Planos</h2>
+    <h3>Comece a cuidar da sua saúde hoje mesmo! Na Academia Mataraca, temos o
+pacote de serviços ideal para que você <br>  alcance seus objetivos de saúde e bem-estar.
+É só comparar e escolher o plano com o melhor <br> custo-benefício.<h3>
+</nav>
+
+
+
+    
